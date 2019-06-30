cc0f69c9fb68fc48ebf180e79f55ed881741db11
marioperera <pereramario19@gmail.com>
2019-06-30 13:29:58 +0530

    borrowing controller added

--- backend/src/main/java/com/codecool/library/repository/BorrowingRepository.java
+++ backend/src/main/java/com/codecool/library/repository/BorrowingRepository.java
@@ -15,7 +15,6 @@ public interface BorrowingRepository extends JpaRepository<Borrowing, Long> {
 
 
 //    List<Borrowing> findBorrowingBySpecimenAndReturnDateIsNull(Specimen specimen);
-
-
+    List<Borrowing> findBorrowingById(Long id);
 
 }
