d09583200d4c547e9bad52b0b860387152d54619
marioperera <pereramario19@gmail.com>
2019-06-30 14:33:29 +0530

    test

---
+++ backend/src/main/java/com/codecool/library/controller/BorrowingController.java
@@ -0,0 +1,58 @@
+package com.codecool.library.controller;
+
+
+import com.codecool.library.error.Error_reply;
+import com.codecool.library.model.Borrowing;
+//import com.codecool.library.model.User;
+import com.codecool.library.repository.BorrowingRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BorrowingController {
+
+    private BorrowingRepository borrowingRepository;
+
+    public BorrowingController(BorrowingRepository borrowingRepository) {
+        this.borrowingRepository = borrowingRepository;
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(method = RequestMethod.GET,value = "/library/Borrowings/{id}")
+    public Borrowing getBorrowingById(@PathVariable("id") Long id) {
+        return borrowingRepository.findById(id).get();
+    }
+
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(method = RequestMethod.GET,value = "/library/Borrowings/")
+    public List<Borrowing> getAllBorrowingById() {
+        return (List<Borrowing>) borrowingRepository.findAll();
+    }
+
+
+    @CrossOrigin(origins ="http://localhost:4200")
+    @DeleteMapping("/library/Borrowings/{id}")
+    public void deleteBook(@PathVariable Long id){
+        borrowingRepository.deleteById(id);
+    }
+
+
+    @CrossOrigin(origins ="http://localhost:4200")
+    @PostMapping("/library/Borrowings")
+    public Error_reply addBorrowing(@RequestBody Borrowing borrowing){
+        borrowingRepository.save(borrowing);
+        return new Error_reply("added");
+//        Long user =borrowing.getUser();
+//        List<Borrowing> borrowings =borrowingRepository.findBorrowingById(user);
+//        int size =borrowings.size();
+//        if (size <3){
+//
+//
+//        }else{
+//            return new Error_reply("limit_exceeded");
+//        }
+//        return new Error_reply("limit_exceeded");
+    }
+}
